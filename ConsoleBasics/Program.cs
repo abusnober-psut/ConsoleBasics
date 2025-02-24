1a94151ad4699eeb59c54a04a169eea96c02ca05
Mohammad Abu Snober <mohammad.abusnober@hotmail.com>
2025-02-24 14:33:37 +0300

    Add project files.

---
+++ ConsoleBasics/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // writing text to the Console Screen
+            Console.WriteLine("Welcome \t \"to\" \nC#");
+
+            // input from the keyboard 
+            int num1=0, num2;
+
+            Console.WriteLine("Enter 2 int number:");
+            num1 = int.Parse( Console.ReadLine() ); // read until the end of the line
+            num2 = Convert.ToInt32( Console.ReadLine());
+
+            int sum = num1 + num2; // addition
+            Console.WriteLine("Sum = " + sum);  // string concate
+            Console.WriteLine($"Sum is {num1+num2}"); // placeholder: variable or expression
+
+        }
+    }
+}
